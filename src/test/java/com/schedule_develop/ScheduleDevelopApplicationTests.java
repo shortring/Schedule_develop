c063755e8173595596022a4c0727ce8ce9b728f8
chan <zinchan1@naver.com>
2025-11-10 14:37:01 +0900

    LV1 프로젝트 생성

---
+++ src/test/java/com/schedule_develop/ScheduleDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
